8645b7c587d5e723c274bab936972bf2a1336cf8
Capitani Clavero Santiago <s.capitani@etec.local>
2023-05-05 09:47:29 -0300

    pañordo

--- FrontEndStore/prueba/src/index.js
+++ FrontEndStore/prueba/src/index.js
@@ -2,10 +2,11 @@
 
 /* Upload images //START// */
 const express  = require('express');
-const ejs = require ("ejs")
 const path = require ('path');
+const multer = require('multer');
+const uuid = require('uuid');
 
-//INitializations 
+//Initializations 
 const app = express();
 
 //Settings
@@ -13,10 +14,34 @@ app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Multer middleware
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'img/public/upload'),
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+app.use(multer({
+    storage,
+    dest: path.join(__dirname, 'img/public/upload'),
+    limits: {fileSize: 1000000},
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpg|jpeg|png|jfif/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if (mimetype && extname){
+            return cb(null, true);
+        }
+        cb("Error: Archivo debe ser una imagen valida");
+    }
+}).single('image'));
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
 //Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(require('./routes/index.routes.js'));
 
 //Start the server
 app.listen(app.get('port'), () => {
